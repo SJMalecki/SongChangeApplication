880853501a6286686a0cc39e27926cafe63403f4
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-11-02 15:59:06 +0100

    Adapt firstMenu() to new playlist menu. Created playlist menu that use ListIterator. Implemented switching to next song.

--- src/pl/sjmprofil/Menu.java
+++ src/pl/sjmprofil/Menu.java
@@ -1,6 +1,8 @@
 package pl.sjmprofil;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Menu {
@@ -8,14 +10,14 @@ public class Menu {
     private Scanner scanner = new Scanner(System.in);
     private PlayList playList = new PlayList("Default playlist");
 
-    public void firstMenu(){
+    public void firstMenu() {
         boolean quit = false;
-        while(!quit){
+        while (!quit) {
             printFirstMenu();
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch(choice){
+            switch (choice) {
                 case 0:
                     quit = true;
                     System.out.println("Closing application...");
@@ -29,55 +31,132 @@ public class Menu {
                 case 3:
                     playList.printPlaylist();
                     break;
+                case 4:
+                    playlistMenu(playList.getUserPlayList());
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+
             }
         }
     }
 
-    public void albumMenu(){
+    public void albumMenu() {
         boolean quit = false;
 
-        while(!quit){
+        while (!quit) {
             printAlbumMenu();
 
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch(choice){
+            switch (choice) {
                 case 0:
-                    quit = true; break;
+                    quit = true;
+                    break;
                 case 1:
-                    System.out.println("Enter artist name");
-                    String artist = scanner.nextLine();
-                    System.out.println("Enter Title");
-                    String albumTitle = scanner.nextLine();
-                    playList.createNewAlbum(artist, albumTitle);
+                    creatingNewAlbumFromMenu();
                     break;
                 case 2:
-                    playList.addSongToAlbum(); break;
+                    playList.addSongToAlbum();
+                    break;
                 case 3:
-                    playList.removeAlbum(); break;
+                    playList.removeAlbum();
+                    break;
                 case 4:
-                    if(playList.printAlbumList()) {
-                        System.out.println("Choose album");
-                        int albumChoice = scanner.nextInt();
-                        scanner.nextLine();
-                        Album album = playList.findAlbum(albumChoice);
-                        if(album != null){
-                            album.removeSong();
-                        }
-                    }
+                    removeSongFromMenu();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
                     break;
             }
         }
     }
 
-    public void selectSong(){
-        Iterator<Song> iterator = playList.getUserPlayList().iterator();
+    public void playlistMenu(LinkedList<Song> playlistOfSongs) {
+        ListIterator<Song> listIterator = playlistOfSongs.listIterator();
+
+        if (playlistOfSongs.isEmpty()) {
+            System.out.println("Playlist is empty");
+            return;
+        } else {
+            System.out.println("Currently playing: " + listIterator.next().getTitle());
+        }
+
+        boolean goingForward = true;
+        boolean quit = false;
+
+        while (!quit) {
+            printPlaylistMenu();
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    if (!goingForward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now playing: " + listIterator.next().getTitle());
+                    } else {
+                        System.out.println("Reached end of list");
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+
+            }
+
+        }
 
 
     }
 
-    public void printAlbumMenu(){
+    public void creatingNewAlbumFromMenu() {
+        System.out.println("Enter artist name");
+        String artist = scanner.nextLine();
+        System.out.println("Enter Title");
+        String albumTitle = scanner.nextLine();
+        playList.createNewAlbum(artist, albumTitle);
+    }
+
+    public boolean removeSongFromMenu() {
+        if (playList.printAlbumList()) {
+            System.out.println("Choose album");
+            int albumChoice = scanner.nextInt();
+            scanner.nextLine();
+            Album album = playList.findAlbum(albumChoice);
+            if (album != null) {
+                album.removeSong();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printPlaylistMenu() {
+        System.out.println("\nPress\n" +
+                "0 - To return\n" +
+                "1 - To go to next song\n" +
+                "2 - To go to previous song\n" +
+                "3 - To print playlist\n" +
+                "4 - To ...\n" +
+                "Chose: ");
+    }
+
+    public void printAlbumMenu() {
         System.out.println("\nPress\n" +
                 "0 - To return\n" +
                 "1 - To create new album\n" +
@@ -86,21 +165,22 @@ public class Menu {
                 "4 - To remove song from album\n");
     }
 
-    public void printFirstMenu(){
+    public void printFirstMenu() {
         System.out.println("\nPress\n" +
                 "1 - To enter album menu\n" +
                 "2 - To add song to playlist\n" +
                 "3 - To show playlist tracks\n" +
+                "4 - To enter playlist menu\n" +
                 "0 - To quit\n" +
                 "Chose: ");
     }
 
-    public void addTestAlbumsToPlaylist(){
+    public void addTestAlbumsToPlaylist() {
         playList.createNewAlbum("Kimi Rikkonen", "Winner takes it all");
         playList.findAlbum("Kimi Rikkonen", "Winner takes it all")
                 .addSongToAlbum("Race", 6.35);
         playList.findAlbum("Kimi Rikkonen", "Winner takes it all")
-                .addSongToAlbum("Coutdown to the start", 2.59);
+                .addSongToAlbum("Countdown to the start", 2.59);
         playList.findAlbum("Kimi Rikkonen", "Winner takes it all")
                 .addSongToAlbum("Unstoppable", 4.51);
         playList.findAlbum("Kimi Rikkonen", "Winner takes it all")
@@ -109,11 +189,11 @@ public class Menu {
         playList.createNewAlbum("Lewis Hammilton", "Beat the time");
         playList.findAlbum(2).addSongToAlbum("Engine on", 1.56);
         playList.findAlbum(2).addSongToAlbum("Fuel vapors", 3.21);
-        playList.addSongToPlaylist(1,1);
-        playList.addSongToPlaylist(1,2);
-        playList.addSongToPlaylist(1,3);
-        playList.addSongToPlaylist(2,1);
-        playList.addSongToPlaylist(2,2);
+        playList.addSongToPlaylist(1, 1);
+        playList.addSongToPlaylist(1, 2);
+        playList.addSongToPlaylist(1, 3);
+        playList.addSongToPlaylist(2, 1);
+        playList.addSongToPlaylist(2, 2);
     }
 
 }
